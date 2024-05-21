0e0bbf5912e58b18332c0d5a6b0ec27ceb087ecc
Tatsuya Nibuno <nibu10.majik@gmail.com>
2024-05-21 21:49:15 +0900

    Media Filesの追加

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "upload",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
